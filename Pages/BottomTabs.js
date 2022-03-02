ac430067ad1a5a915c1aecec4973c4e4e3cda0d6
Kieran O'Sullivan <kieranosullivan@ufl.edu>
2022-03-02 18:00:04 -0500

    Starting page

--- Pages/BottomTabs.js
+++ Pages/BottomTabs.js
@@ -2,18 +2,56 @@ import React from "react";
 import { StyleSheet, View,Text } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import StartPage from "./StartPage";
-import AddItem from"./AddItem";
 import PantryPage from "./PantryPage";
+//import FridgePage from "./FridgePage";
+
 import GroceryPage from "./GroceryPage";
+import AddItem from"./AddItem";
 import ExpirationPage from "./ExpirationPage";
+import { Ionicons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
 
 const Tab = createBottomTabNavigator();
 
-/*<Tab.Screen name = 'Expiration' component={ExpirationPage} />*/
+
 
 const BottomTabs = () => {
     return(
-        <Tab.Navigator >
+        <Tab.Navigator
+            screenOptions= {({route}) => ({
+                headerStyle: { backgroundColor: '#c4ffc9' },
+                tabBarActiveTintColor: 'black',
+                tabBarInactiveTintColor: 'black',
+                tabBarActiveBackgroundColor: '#c4ffc9',
+                tabBarInactiveBackgroundColor: '#c4ffc9',
+                tabBarShowLabel: true,
+                tabBarIcon: ({focused}) => {
+                    let iconName;
+                    if(route.name === 'Home') {
+                        iconName = 'home';
+                    }
+                    else if(route.name === 'Grocery') {
+                        iconName = focused ? 'cart' : 'cart-outline';
+                    }
+                    else if(route.name === 'Add Item') {
+                        iconName = focused ? 'hourglass' : 'hourglass-outline';
+                    }
+                    else if(route.name === 'Pantry') {
+                        iconName = focused ? 'add-circle' : 'add-circle-outline';
+                    }
+                    return(
+                        <Ionicons
+                        name = {iconName}
+                        size = {32}
+                        />
+                    )
+                },
+                tabBarOptions: {
+                   showIcon: true
+                },
+            })}
+        >
             <Tab.Screen name = 'Home' component={StartPage} />
             <Tab.Screen name = 'Pantry' component={PantryPage} />
             <Tab.Screen name = 'Grocery' component={GroceryPage} />
@@ -23,3 +61,5 @@ const BottomTabs = () => {
 
 }
 export default BottomTabs;
+
+
--- Pages/StartPage.js
+++ Pages/StartPage.js
@@ -16,15 +16,183 @@ import {
   Button,
   useColorScheme,
   View,
+  TouchableOpacity,
 } from 'react-native';
+import {SearchBar} from 'react-native-elements';
+
+var size = 150;
+var t_b = 100;
+var l_r = 30;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  myPantryButton: {
+    position: 'absolute',
+    width: size,
+    height: size,
+    top: t_b,
+    left: l_r,
+    //justifyContent: 'center',
+    //alignItems: 'center',
+    padding: 10,
+    borderRadius: 100,
+    backgroundColor: '#c1d4d3',
+  },
+  myFridgeButton: {
+    position: 'absolute',
+    width: size,
+    height: size,
+    top: t_b,
+    right: l_r,
+    //justifyContent: 'center',
+    //alignItems: 'center',
+    padding: 40,
+    borderRadius: 100,
+    backgroundColor: '#c4c991',
+  },
+  myFreezerButton: {
+      position: 'absolute',
+      width: size,
+      height: size,
+      bottom: t_b + 80,
+      left: l_r,
+      //justifyContent: 'center',
+      //alignItems: 'center',
+      padding: 10,
+      borderRadius: 100,
+      backgroundColor: '#9faad4',
+    },
+  mySpicesButton: {
+        position: 'absolute',
+        width: size,
+        height: size,
+        bottom: t_b + 80,
+        right: l_r,
+        //justifyContent: 'center',
+        //alignItems: 'center',
+        padding: 10,
+        borderRadius: 100,
+        backgroundColor: '#e69b97',
+      },
+  sectionButton:{
+      marginRight:40,
+      marginLeft:40,
+      marginTop:450,
+      paddingTop:40,
+      paddingBottom:10,
+      paddingRight: 20,
+      paddingLeft:20,
+      backgroundColor:'#284d2f',
+      borderRadius:10,
+      borderWidth: 1,
+      borderColor: '#000000'
+    }
+});
+
 
 const StartPage = ({navigation}) => {
 
   return (
-  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    <Text>Mi Cocina</Text>
+  <View style={{
+            flex: 1,
+                 //alignItems: 'center',
+                 //justifyContent: 'center'
+            }}
+            >
+    <SearchBar
+            style={{
+                position: 'absolute',
+                top: 15
+            }}
+            placeholder="Type Here..."
+            //onChangeText={updateSearch}
+            //value={search}
+          />
+
+    <TouchableOpacity
+              style={styles.sectionButton}
+              onPress={() => navigation.navigate('Pantry')}
+              underlayColor='#fff'>
+              <Text style={{
+                position: 'absolute',
+                right: 63,
+                bottom: 10,
+                fontWeight: 'bold',
+                fontSize: 24,
+              }}>
+              View All Sections</Text>
+
+     </TouchableOpacity>
+
+    <React.Fragment>
+          <Text style={{
+            position: 'absolute',
+            top: 250,
+            left: l_r + 40,
+            fontWeight: 'bold'
+          }}>
+            MY PANTRY
+          </Text>
+    </React.Fragment>
+
+    <React.Fragment>
+          <Text style={{
+            position: 'absolute',
+            top: 250,
+            right: l_r + 40,
+            fontWeight: 'bold'
+          }}>
+            MY FRIDGE
+          </Text>
+    </React.Fragment>
+
+    <React.Fragment>
+          <Text style={{
+            position: 'absolute',
+            bottom: 155,
+            left: l_r + 35,
+            fontWeight: 'bold'
+          }}>
+            MY FREEZER
+          </Text>
+    </React.Fragment>
+
+    <React.Fragment>
+          <Text style={{
+            position: 'absolute',
+            bottom: 155,
+            right: l_r + 40,
+            fontWeight: 'bold',
+            fontFamily: 'Cochin'
+          }}>
+            MY SPICES
+          </Text>
+    </React.Fragment>
+
+
+    <TouchableOpacity onPress={() => navigation.navigate('Pantry')}
+                  style={styles.myPantryButton}>
+    </TouchableOpacity>
+
+    <TouchableOpacity onPress={() => navigation.navigate('Pantry')}
+                style={styles.myFreezerButton}>
+    </TouchableOpacity>
+
+    <TouchableOpacity onPress={() => navigation.navigate('Pantry')}
+                style={styles.myFridgeButton}>
+    </TouchableOpacity>
+
+    <TouchableOpacity onPress={() => navigation.navigate('Pantry')}
+                style={styles.mySpicesButton}>
+    </TouchableOpacity>
+
   </View>
   );
 };
 
 export default StartPage;
+
