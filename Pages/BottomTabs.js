69838c0da24ef704866bd01110a4aafd03a8907e
Samantha Martinez <martinezs@ufl.edu>
2022-03-03 02:18:04 -0500

    Fixed Revert Mistake
    This should be the original before I reverted it.

---
+++ Pages/BottomTabs.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { StyleSheet, View,Text } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import StartPage from "./StartPage";
+import AddItem from"./AddItem";
+import PantryPage from "./PantryPage";
+import GroceryPage from "./GroceryPage";
+import ExpirationPage from "./ExpirationPage";
+
+const Tab = createBottomTabNavigator();
+
+/*<Tab.Screen name = 'Expiration' component={ExpirationPage} />*/
+
+const BottomTabs = () => {
+    return(
+        <Tab.Navigator >
+            <Tab.Screen name = 'Home' component={StartPage} />
+            <Tab.Screen name = 'Pantry' component={PantryPage} />
+            <Tab.Screen name = 'Grocery' component={GroceryPage} />
+            <Tab.Screen name = 'Add Item' component={AddItem} />
+        </Tab.Navigator>
+    )
+
+}
+export default BottomTabs;
---
+++ Pages/ExpirationPage.js
@@ -0,0 +1,39 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+ import React from 'react';
+ import {
+   SafeAreaView,
+   ScrollView,
+   StatusBar,
+   StyleSheet,
+   Text,
+   Button,
+   useColorScheme,
+   View,
+ } from 'react-native';
+ 
+ const styles = StyleSheet.create({
+   center: {
+     alignItems: 'center'
+   },
+ 
+   green:  {
+     color: "#90EE90"
+   }
+ })
+ 
+ const ExpirationPage = () => {
+   return (
+   <View style={[styles.center, {top: 50}]}>
+     <Text>Expiration Page</Text>
+   </View>
+   );
+ };
+ 
+ export default ExpirationPage;
