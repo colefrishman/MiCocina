28a85266c17a156d9a586e00b3a6aef9262ce58a
Cole Frishman <awesomeeloc@gmail.com>
2022-03-04 10:15:05 -0500

    Deleted unworking start button

--- Pages/StartPage.js
+++ Pages/StartPage.js
@@ -23,7 +23,6 @@ const StartPage = ({navigation}) => {
   return (
   <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
     <Text>Mi Cocina</Text>
-    <Button title='Start' onPress={() => navigation.navigate("Pick")}>Begin</Button>
   </View>
   );
 };
